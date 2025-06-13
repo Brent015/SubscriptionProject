d118acd6d25454a96af69ca045749f0a790b2666
brent <markbrentgonzales@gmail.com>
2025-06-13 13:14:54 +0800

    delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -39,3 +39,52 @@ export const getUser = async (req, res, next) => {
         next(error);
     }
 }
+export const deleteUser = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    
+    try {
+        const userId = req.params.id;
+        
+        // Check if user is trying to delete their own account or if they're an admin
+        if (req.user._id.toString() !== userId && req.user.role !== 'admin') {
+            const error = new Error("Unauthorized: You can only delete your own account");
+            error.statusCode = 403;
+            throw error;
+        }
+
+        // Find the user to delete
+        const userToDelete = await User.findById(userId);
+        if (!userToDelete) {
+            const error = new Error("User not found");
+            error.statusCode = 404;
+            throw error;
+        }
+
+        // Prevent admin from deleting themselves (optional safety check)
+        if (userToDelete.role === 'admin' && req.user._id.toString() === userId) {
+            const error = new Error("Admin cannot delete their own account");
+            error.statusCode = 400;
+            throw error;
+        }
+
+        // Delete all subscriptions associated with this user
+        await Subscription.deleteMany({ user: userId }, { session });
+        
+        // Delete the user
+        await User.findByIdAndDelete(userId, { session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(200).json({
+            success: true,
+            message: "User and associated data deleted successfully"
+        });
+        
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import {authorize} from "../middlewares/auth.middleware.js";
 import { requireAdmin } from "../middlewares/admin.middleware.js";
-import { getUser, getUsers } from "../controllers/user.controller.js";
+import { getUser, getUsers, deleteUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
@@ -17,5 +17,8 @@ userRouter.post("/", (req, res) => res.send("CREATE new users"));
 userRouter.put("/:id", (req, res) => res.send("UPDATE users"));
 
 userRouter.delete("/:id", (req, res) => res.send("DELETE a user"));
+
+// Delete user (hard delete - permanently removes user and data)
+userRouter.delete("/:id", authorize, deleteUser);
 
 export default userRouter;
